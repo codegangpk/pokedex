8ccc4e6df827a8a7bf6a3395fb4e1239921b03b6
Paul Kim <parahop94@gmail.com>
2020-12-18 22:06:06 +0900

    move folder structures

--- Pokedex/Views/SearchView.swift
+++ Pokedex/Views/SearchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SearchView.swift
 //  Pokedex
 //
 //  Created by Paul Kim on 2020/12/18.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SearchView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SearchView()
     }
 }
