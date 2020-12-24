27e903d52ee75730c077c1a3c864f66b7702cfd0
Paul Kim <parahop94@gmail.com>
2020-12-24 13:02:04 +0900

    do not dismiss keyboard in SearchViewController after pushing into PokemonViewController

--- Pokedex/Application/Presentation/ViewControllers/SearchViewController.swift
+++ Pokedex/Application/Presentation/ViewControllers/SearchViewController.swift
@@ -89,7 +89,6 @@ extension SearchViewController: UITableViewDelegate {
         let row = dataSource.item(for: indexPath)
         switch row {
         case .pokemon(let viewModel):
-            navigationItem.titleView?.endEditing(true)
             PokemonViewController.push(in: self, pokemonSearchResult: viewModel.pokemonSearchResult)
         }
     }
