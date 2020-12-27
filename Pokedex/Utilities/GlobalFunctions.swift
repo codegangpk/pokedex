9522a0d9159fc4bed7cc9705e8589a9addd2a8c2
Paul Kim <parahop94@gmail.com>
2020-12-27 17:16:58 +0900

    overshadow print function to print for debug mode only

--- Pokedex/Utilities/GlobalFunctions.swift
+++ Pokedex/Utilities/GlobalFunctions.swift
@@ -11,8 +11,14 @@ func afterDelay(_ seconds: Double = 0, run: @escaping () -> Void) {
     DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: run)
 }
 
-func print(_ items: Any...) {
+func print(_ object: Any) {
     #if DEBUG
-    Swift.print(items)
+    Swift.print(object)
+    #endif
+}
+
+func print(_ objects: Any...) {
+    #if DEBUGa
+    Swift.print(objects)
     #endif
 }
